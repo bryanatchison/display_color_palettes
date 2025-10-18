b27cc72bac764051464c4e1d56eacd2008140f77
milan <atchison.dev@gmail.com>
2025-10-18 09:29:31 -0500

    Add a single paletete and display with matplotlib.pyplot.

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+import matplotlib.pyplot as plt
+
 def main():
-    print("Hello from display-color-palettes!")
+
+    # Define a color palette
+    colors = [
+        "#0D3B66", 
+        "#FAF0CA", 
+        "#F4D35E", 
+        "#EE964B", 
+        "#F95738"
+        ]
+
+    # Create a figure
+    plt.figure(figsize=(8, 2))
+
+    # Display the colors
+    for i, color in enumerate(colors):
+        plt.fill_between([i, i + 1], 0, 1, color=color)
+
+    # Set the title and remove axes
+    plt.title('My new palette')
+    plt.axis('off')
+    plt.show()
 
 
 if __name__ == "__main__":
