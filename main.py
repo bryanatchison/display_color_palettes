4854cc6912f3fb1095664f9fc497be013ce10db1
milan <atchison.dev@gmail.com>
2025-10-18 23:00:06 -0500

    Connect palette JSON to display for first palette in JSON.

--- main.py
+++ main.py
@@ -7,21 +7,16 @@ def main():
         with open('Coolors Palettes.json', 'r') as file:
             content = file.read()
             data = json.loads(content)
-            print(data)
+            colors = []
+            for color_dyad in data['palettes'][0]['palette_colors']:
+                colors.append('#' + color_dyad[1])
+
+            print(colors)
         
     except FileNotFoundError:
         print("The file was not found.")
     except json.JSONDecodeError:
         print("Error decoding JSON.")
-
-    # Define a color palette
-    colors = [
-        "#0D3B66", 
-        "#FAF0CA", 
-        "#F4D35E", 
-        "#EE964B", 
-        "#F95738"
-        ]
 
     # Create a figure
     plt.figure(figsize=(8, 2))
@@ -31,7 +26,7 @@ def main():
         plt.fill_between([i, i + 1], 0, 1, color=color)
 
     # Set the title and remove axes
-    plt.title('My new palette')
+    plt.title(data['palettes'][0]['palette_name'])
     plt.axis('off')
     plt.show()
 
